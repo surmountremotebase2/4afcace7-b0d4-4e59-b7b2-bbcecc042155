dd7d5480027c1bf0a939b9568da517a2249dc002
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-21 17:05:14 -0400

    User Marco Ellwanger saved changes to main.py at 04/21/2024, 21:05:13 UTC time.

--- cc5626ac-552a-42c9-b362-316cc92164ac/main.py
+++ cc5626ac-552a-42c9-b362-316cc92164ac/main.py
@@ -39,7 +39,7 @@ class KevinStrategy(Strategy):
         threshold = buy_5l_low_at(data, ticker):
         if threshold > -1.0:
             if data["holdings"][ticker] >= 0:
-                log("[buy_5l_low_at] buying " + ticker + " at: " + data[-1]["close"]) + " (signal threshold: " + threshold + ")");
+                log("[buy_5l_low_at] buying " + ticker + " at: " + data[-1]["close"] + " (signal threshold: " + threshold + ")");
                 stake = min(1, data["holdings"][ticker]+0.1)
                 log("[buy_5l_low_at] new stake for " + ticker + ": " + stake);
             else:
