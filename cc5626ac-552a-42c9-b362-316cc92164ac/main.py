7793498f6610af11a30609f38e4f97b6a76b2933
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-04-21 17:40:20 -0400

    User Marco Ellwanger saved changes to main.py at 04/21/2024, 21:40:20 UTC time.

--- cc5626ac-552a-42c9-b362-316cc92164ac/main.py
+++ cc5626ac-552a-42c9-b362-316cc92164ac/main.py
@@ -1,5 +1,91 @@
 from surmount.base_class import Strategy, TargetAllocation
 
+# trueinrow(C < C1, 5) = 5 and L < L1 and X > L1 + ((H1 - L1) / 2) with X = signal threshold
+def buy_5l_close_at(data, ticker):
+    close1 = data[-2][ticker]["close"]
+    close2 = data[-3][ticker]["close"]
+    close3 = data[-4][ticker]["close"]
+    close4 = data[-5][ticker]["close"]
+    close5 = data[-6][ticker]["close"]
+    high = data[-1][ticker]["high"]
+    low = data[-1][ticker]["low"]
+    low1 = data[-2][ticker]["high"]
+    high1 = data[-2][ticker]["high"]
+
+    if close1 < close2 and close1 < close3 and close1 < close4 and close1 < close5 and low < low1:
+        threshold = low1 + ((high1 - low1) / 2)
+        # return threshold for confirmation if below day high
+        if high > threshold:
+            return threshold
+        else:
+            return -1.0
+    else:
+        return -1.0
+
+# l = minl5 and X > L1 + ((H1 - L1) / 2) with X = signal threshold
+def buy_5l_low_at(data, ticker):
+    low = data[-1][ticker]["low"]
+    low1 = data[-2][ticker]["low"]
+    low2 = data[-3][ticker]["low"]
+    low3 = data[-4][ticker]["low"]
+    low4 = data[-5][ticker]["low"]
+    high = data[-1][ticker]["high"]
+    high1 = data[-2][ticker]["high"]
+
+    if low < low1 and low < low2 and low < low3 and low < low4:
+        threshold = low1 + ((high1 - low1) / 2)
+        # return threshold for confirmation if below day high
+        if high > threshold:
+            return threshold
+        else:
+            return -1.0
+    else:
+        return -1.0
+
+
+# trueinrow(C < C1, 5) = 5 and H > H1 and X > L1 + ((H1 - L1) / 2) with X = signal threshold
+def sell_5h_close_at(data, ticker):
+    close1 = data[-2][ticker]["close"]
+    close2 = data[-3][ticker]["close"]
+    close3 = data[-4][ticker]["close"]
+    close4 = data[-5][ticker]["close"]
+    close5 = data[-6][ticker]["close"]
+    low = data[-1][ticker]["low"]
+    low1 = data[-2][ticker]["low"]
+    high1 = data[-2][ticker]["high"]
+    high = data[-1][ticker]["high"]
+
+    if close1 >= close2 and close1 >= close3 and close1 >= close4 and close1 >= close5 and high > high1:
+        threshold = high1 - ((high1 - low1) / 2)
+        # return threshold for confirmation if above day low
+        if low < threshold:
+            return threshold
+        else:
+            return -1.0
+    else:
+        return -1.0
+
+
+# h = maxh5 and X < H1 - ((H1 - L1) / 2) with X = signal threshold
+def sell_5h_high_at(data, ticker):
+    high = data[-1][ticker]["high"]
+    low = data[-1][ticker]["low"]
+    high1 = data[-2][ticker]["high"]
+    high2 = data[-3][ticker]["high"]
+    high3 = data[-4][ticker]["high"]
+    high4 = data[-5][ticker]["high"]
+    low1 = data[-2][ticker]["low"]
+
+    if high >= high1 and high >= high2 and high >= high3 and high >= high4:
+        threshold = high1 - ((high1 - low1) / 2)
+        # return threshold for confirmation if above day low
+        if low < threshold:
+            return threshold
+        else:
+            return -1.0
+    else:
+        return -1.0
+
 class TradingStrategy(Strategy):
 
     def __init__(self):
@@ -17,97 +103,11 @@ class TradingStrategy(Strategy):
             # shorting seemingly not supported
         return assets
 
-    # trueinrow(C < C1, 5) = 5 and L < L1 and X > L1 + ((H1 - L1) / 2) with X = signal threshold
-    def buy_5l_close_at(data, ticker):
-        close1 = data[-2][ticker]["close"]
-        close2 = data[-3][ticker]["close"]
-        close3 = data[-4][ticker]["close"]
-        close4 = data[-5][ticker]["close"]
-        close5 = data[-6][ticker]["close"]
-        high = data[-1][ticker]["high"]
-        low = data[-1][ticker]["low"]
-        low1 = data[-2][ticker]["high"]
-        high1 = data[-2][ticker]["high"]
-
-        if close1 < close2 and close1 < close3 and close1 < close4 and close1 < close5 and low < low1:
-            threshold = low1 + ((high1 - low1) / 2)
-            # return threshold for confirmation if below day high
-            if high > threshold:
-                return threshold
-            else:
-                return -1.0
-        else:
-            return -1.0
-
-    # l = minl5 and X > L1 + ((H1 - L1) / 2) with X = signal threshold
-    def buy_5l_low_at(data, ticker):
-        low = data[-1][ticker]["low"]
-        low1 = data[-2][ticker]["low"]
-        low2 = data[-3][ticker]["low"]
-        low3 = data[-4][ticker]["low"]
-        low4 = data[-5][ticker]["low"]
-        high = data[-1][ticker]["high"]
-        high1 = data[-2][ticker]["high"]
-
-        if low < low1 and low < low2 and low < low3 and low < low4:
-            threshold = low1 + ((high1 - low1) / 2)
-            # return threshold for confirmation if below day high
-            if high > threshold:
-                return threshold
-            else:
-                return -1.0
-        else:
-            return -1.0
-
-
-    # trueinrow(C < C1, 5) = 5 and H > H1 and X > L1 + ((H1 - L1) / 2) with X = signal threshold
-    def sell_5h_close_at(data, ticker):
-        close1 = data[-2][ticker]["close"]
-        close2 = data[-3][ticker]["close"]
-        close3 = data[-4][ticker]["close"]
-        close4 = data[-5][ticker]["close"]
-        close5 = data[-6][ticker]["close"]
-        low = data[-1][ticker]["low"]
-        low1 = data[-2][ticker]["low"]
-        high1 = data[-2][ticker]["high"]
-        high = data[-1][ticker]["high"]
-
-        if close1 >= close2 and close1 >= close3 and close1 >= close4 and close1 >= close5 and high > high1:
-            threshold = high1 - ((high1 - low1) / 2)
-            # return threshold for confirmation if above day low
-            if low < threshold:
-                return threshold
-            else:
-                return -1.0
-        else:
-            return -1.0
-
-
-    # h = maxh5 and X < H1 - ((H1 - L1) / 2) with X = signal threshold
-    def sell_5h_high_at(data, ticker):
-        high = data[-1][ticker]["high"]
-        low = data[-1][ticker]["low"]
-        high1 = data[-2][ticker]["high"]
-        high2 = data[-3][ticker]["high"]
-        high3 = data[-4][ticker]["high"]
-        high4 = data[-5][ticker]["high"]
-        low1 = data[-2][ticker]["low"]
-
-        if high >= high1 and high >= high2 and high >= high3 and high >= high4:
-            threshold = high1 - ((high1 - low1) / 2)
-            # return threshold for confirmation if above day low
-            if low < threshold:
-                return threshold
-            else:
-                return -1.0
-        else:
-            return -1.0
-
     def run(self, data):
         allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
         d = data["ohlcv"]
         for ticker in self.tickers:
-            threshold = self.buy_5l_close_at(d, ticker);
+            threshold = buy_5l_close_at(d, ticker);
             if threshold > -1.0:
                 if data["holdings"][ticker] >= 0:
                     log("[buy_5l_close_at] buy " + ticker + " at: " + data[-1]["close"] + " (signal threshold: " + threshold + ")");
